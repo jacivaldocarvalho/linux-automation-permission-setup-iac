3e57d4755be80a1236733c505dc145d24706438c
jacivaldocarvalho <jacivaldocarvalho@gmail.com>
2023-01-19 15:51:33 +0000

    coreção de -crypt para -1

--- criar_user.sh
+++ criar_user.sh
@@ -2,13 +2,13 @@
 
 echo "Criando usuários do sistema"
 
-useradd guest10 -c "Usuário convidado" -s /bin/bash -m -p $(openssl passwd -crypt Senha123)
+useradd guest10 -c "Usuário convidado" -s /bin/bash -m -p $(openssl passwd -1 Senha123)
 passwd guest10 -e
 
-useradd guest11 -c "Usuário convidado" -s /bin/bash -m -p $(openssl passwd -crypt Senha123)
+useradd guest11 -c "Usuário convidado" -s /bin/bash -m -p $(openssl passwd -1 Senha123)
 passwd guest11 -e
 
-useradd guest12 -c "Usuário convidado" -s /bin/bash -m -p $(openssl passwd -crypt Senha123)
+useradd guest12 -c "Usuário convidado" -s /bin/bash -m -p $(openssl passwd -1 Senha123)
 passwd guest12 -e
 
 echo "Finalizado ..."
---
+++ iac-02.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Cria apenas os usuários do projeto
+echo "Criando os usuários ... "
+
+echo "Do grupo ADMINISTRADOR"
+useradd carlos -m -s /bin/bash -p $(openssl passwd -1 Senha123) -G GRP_ADM
+useradd maria -m -s /bin/bash -p $(openssl passwd -1 Senha123) -G GRP_ADM
+useradd joao -m -s /bin/bash -p $(openssl passwd -1 Senha123) -G GRP_ADM
+
+echo "Do grupo VENDAS"
+useradd debora -m -s /bin/bash -p $(openssl passwd -1 Senha123) -G GRP_VEN
+useradd sebastiao -m -s /bin/bash -p $(openssl passwd -1 Senha123) -G GRP_VEN
+useradd roberto -m -s /bin/bash -p $(openssl passwd -1 Senha123) -G GRP_VEN
+
+echo "Do grupo SECRETARIADO"
+useradd josefina -m -s /bin/bash -p $(openssl passwd -1 Senha123) -G GRP_SEC
+useradd amanda -m -s /bin/bash -p $(openssl passwd -1 Senha123) -G GRP_SEC
+useradd rogerio -m -s /bin/bash -p $(openssl passwd -1 Senha123) -G GRP_SEC
