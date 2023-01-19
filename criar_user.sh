9a7b8b4f52f240b5c3f2aaecd9d15a04287ea42c
jacivaldocarvalho <jacivaldocarvalho@gmail.com>
2023-01-19 10:20:26 +0000

    iac-01 v1.1

---
+++ criar_user.sh
@@ -0,0 +1,15 @@
+#cria usuarios
+#!/bin/bash
+
+echo "Criando usuários do sistema"
+
+useradd guest10 -c "Usuário convidado" -s /bin/bash -m -p $(openssl passwd -crypt Senha123)
+passwd guest10 -e
+
+useradd guest11 -c "Usuário convidado" -s /bin/bash -m -p $(openssl passwd -crypt Senha123)
+passwd guest11 -e
+
+useradd guest12 -c "Usuário convidado" -s /bin/bash -m -p $(openssl passwd -crypt Senha123)
+passwd guest12 -e
+
+echo "Finalizado ..."
