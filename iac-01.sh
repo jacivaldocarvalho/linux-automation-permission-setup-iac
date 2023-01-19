5b2a84b8d3de002c8b4df59655a93e4799a9004d
jacivaldocarvalho <jacivaldocarvalho@gmail.com>
2023-01-19 10:05:22 +0000

    iac-01 v1.0

---
+++ iac-01.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+echo "Criando diretórios!!"
+
+mkdir /publico
+mkdir /adm
+mkdir /ven
+mkdir /sec
+
+
+echo "Criando os Grupos ADM VEN e SEC!!"
+
+groupadd GRP_ADM
+groupadd GRP_VEN
+groupadd GRP_SEC
+
+
+echo "Criando os usuários ... "
+
+echo "Do grupo ADMINISTRADOR"
+useradd carlos -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_ADM
+useradd maria -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_ADM
+useradd joao -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_ADM
+
+echo "Do grupo VENDAS"
+useradd debora -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_VEN
+useradd sebastiao -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_VEN
+useradd roberto -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_VEN
+
+echo "Do grupo SECRETARIADO"
+useradd josefina -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_SEC
+useradd amanda -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_SEC
+useradd rogerio -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_SEC
+
+
+echo "Especificando permissões dos diretórios..."
+
+chown root:GRP_ADM /adm
+chown root:GRP_VEN /ven
+chown root:GRP_SEC /sec
+
+
+chmod 770 /adm
+chmod 770 /ven
+chmod 770 /sec
+chmod 777 /publico
+
+
+echo "Script finalizado ... !!"
